1c62576b897977e4cf978d2495c96b7057391866
Kam Brown <kbrown8055@outlook.com>
2019-08-16 16:24:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,17 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase();
-}
-function sayHiToGrandma(string) {
-  return string.toUpperCase
-  
-  function sayHiToGrandma(string) {
-    if (string.toLowerCase() === string) {
-      return “I can’t hear you!” ;
-}   if (string.toUpperCase() === string ) {
-      return “YES INDEED!”
-}   if (string === `I love you, Grandma.` ) {
-      return “I love you, too.”;
+  if (string.toLowerCase() === string) {
+    return “I can’t hear you!” ;
+} if (string.toUpperCase() === string ) {
+    return “YES INDEED!”
+} if (string === `I love you, Grandma.` ) {
+    return “I love you, too.”;
 }}
 }
